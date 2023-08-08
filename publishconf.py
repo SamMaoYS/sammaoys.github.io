47666d45308dd823bdd85a0848c1a15abd15b0da
Yongsen Mao <sammaoys@gmail.com>
2023-08-08 17:16:19 +0800

    update publish config

--- publishconf.py
+++ publishconf.py
@@ -8,12 +8,12 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://sammaoys.github.io'
-RELATIVE_URLS = False
-
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
+
+M_CSS_FILES = ['https://fonts.googleapis.com/css?family=Source+Code+Pro:400,400i,600%7CSource+Sans+Pro:400,400i,600,600i&subset=latin-ext',
+               'static/m-dark.compiled.css']
+THEME_STATIC_PATHS = ['static/m-dark.compiled.css']
 
 # Following items are often useful when publishing
 
